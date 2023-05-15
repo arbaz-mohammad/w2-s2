96088e7c4c23cf4a67a02d57d6224e1e47dd515c
arbaz-mohammad <127589977+arbaz-mohammad@users.noreply.github.com>
2023-05-15 21:04:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("hello")
+var str =  "skill safari"
+console.log(str)
+console.log(str.indexOf("i"))
+// length
+console.log(str.length)
+console.log("Captain America: (sitting on a log) You know, if you're going to stay out here, you might want to try working on your people skills. . \n Iron Man: (sipping a drink) Yeah, well, what are you gonna do? Go to space, steal a moon rock?.\nCaptain America: (smirking) Been there, done that. \nIron Man: (laughs) How was it?")
+
+// bmi
+ var weight=12
+ var height=2
+  const bmi= weight / (height*height)
+  console.log("your bmi is "+bmi)
